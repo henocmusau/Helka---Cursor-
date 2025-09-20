5ff104e136e908d4f06a556ae7d9e5f5d6eee728
Henoc Musau <88394951+henocmusau@users.noreply.github.com>
2025-09-20 05:51:01 +0100

    feat: Test successfully Alert dialog within Sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+         <SidebarProvider>
+      <AppSidebar />
+      <SidebarTrigger />
         {children}
+        </SidebarProvider>
       </body>
     </html>
   );
