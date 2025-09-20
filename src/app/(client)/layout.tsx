c43c0b99cb61844c41e3a9221ff02aa106e57702
Henoc Musau <88394951+henocmusau@users.noreply.github.com>
2025-09-20 12:08:18 +0100

    feat: customize shadcn comoonents with tweakcn

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -1,13 +1,18 @@
 // import type { Metadata } from "next";
 
+import { NavigationMenuDemo } from "@/components/navigation-menu";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div
+    <div className="w-full h-full"
     >
+      <header className="w-full flex items-center justify-center">
+        <NavigationMenuDemo />
+      </header>
       {children}
     </div>
   );
